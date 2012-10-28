3d7a8583603c0bb29fd5b61cff0598644613f2d9
jogam5 <jogam5@yahoo.com.mx>
2012-10-28 17:08:57 -0600

    Facebook Key Updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '439343236107925', 'fb1c82f1f5893548e750e18e0b67362e',
+  provider :facebook, '235628993153454', '6dc90b8b268f2643ebd5b074a88db7c8',
   		 	:scope => 'email,user_birthday,read_stream, publish_stream'
   #provider :twitter, "bh0NqOQkC1Hi1M8ib37jQ", "yEZGPrpWeXzRdYeqLuxsNs2IwkW961Lc6f32HQFKU"
 end
