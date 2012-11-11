a7f81509b997a94772877eb2466f8e3542f69c5b
jogam5 <jogam5@yahoo.com.mx>
2012-11-11 02:17:11 -0600

    Fonts, Mailchimp, BeforeAdmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_url
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to user_path(current_user), :alert => exception.message
 	exception.default_message = "blage"
+  end
+
+  def check_url
+	  url = request.url
+	  if url.include?('soxialit.herokuapp.com')
+	    redirect_to ('http://www.soxialit.com')
+	  elsif url.include?('soxialit.com')
+	    redirect_to ('http://www.soxialit.com')       
+	  end    
   end
 end
