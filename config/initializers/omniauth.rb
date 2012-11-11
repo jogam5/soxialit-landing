242930ad5ed0a00fed02fa74c3e4073aa9145171
jogam5 <jogam5@yahoo.com.mx>
2012-11-11 01:38:13 -0600

    SSL2 and Fonts

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["APP_ID"], ENV["APP_SECRET"],
-  		 	:scope => 'email, user_birthday, user_location, publish_stream, read_friendlists'
+  		 	:scope => 'email, user_birthday, user_location, publish_stream, read_friendlists',
+  		 	:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
