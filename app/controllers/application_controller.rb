c904be0eafa4f06294fb2737d9d00c64383d9d6f
jogam5 <jogam5@yahoo.com.mx>
2012-12-13 22:36:52 -0600

    www hack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_uri
+
+  def check_uri
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to user_path(current_user), :alert => exception.message
