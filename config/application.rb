b6800fb488112ed5ae57cdf5591195f39a9a8f49
jogam5 <jogam5@yahoo.com.mx>
2012-12-14 00:48:26 -0600

    Added require

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ end
 module DeviseFacebook
   class Application < Rails::Application
 
+    require 'rack/rewrite'
     config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
         r301 %r{.*}, 'http://www.soxialit.com$&', 
             :if => Proc.new {|rack_env| rack_env['SERVER_NAME'] == 'soxialit.com'}
