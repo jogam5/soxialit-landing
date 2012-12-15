28dca82793d6e99d8191927802d75a25647df933
julio <julio336@hotmail.com>
2012-12-15 11:34:56 -0600

    User signed in

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,37 +45,38 @@ class ProductsController < ApplicationController
       @products = avoid_nil(@user.products.all)
       user_cp = find_user_product(@product)
       
-      if current_user.direction.nil? || user_cp.nil?
-      else
-         user_product_cp = find_user_product(@product)
-         current_user_cp = current_user.direction.zipcode
-         logger.debug "#{user_product_cp}\n\n\n\n\n\n"
-         logger.debug "#{current_user_cp}\n\n\n\n\n\n"
-         if @product.tipo_envio == "sobre"
-             url = "http://rastreo2.estafeta.com:7001/Tarificador/admin/TarificadorAction.do?dispatch=doGetTarifas&cCodPosOri=#{user_product_cp}&cCodPosDes=#{current_user_cp}&cTipoEnvio=#{@product.tipo_envio}&cIdioma=Esp"
-          else
-             url = "http://rastreo2.estafeta.com:7001/Tarificador/admin/TarificadorAction.do?dispatch=doGetTarifas&cCodPosOri=#{user_product_cp}&cCodPosDes=#{current_user_cp}&cTipoEnvio=#{@product.tipo_envio}&cIdioma=Esp&nPeso=#{@product.peso}&nLargo=#{@product.largo}&nAncho=#{@product.ancho}&nAlto=#{@product.alto}"
+      if signed_in?
+         if current_user.direction.nil? || user_cp.nil?
+         else
+            user_product_cp = find_user_product(@product)
+            current_user_cp = current_user.direction.zipcode
+            logger.debug "#{user_product_cp}\n\n\n\n\n\n"
+            logger.debug "#{current_user_cp}\n\n\n\n\n\n"
+            if @product.tipo_envio == "sobre"
+                url = "http://rastreo2.estafeta.com:7001/Tarificador/admin/TarificadorAction.do?dispatch=doGetTarifas&cCodPosOri=#{user_product_cp}&cCodPosDes=#{current_user_cp}&cTipoEnvio=#{@product.tipo_envio}&cIdioma=Esp"
+             else
+                url = "http://rastreo2.estafeta.com:7001/Tarificador/admin/TarificadorAction.do?dispatch=doGetTarifas&cCodPosOri=#{user_product_cp}&cCodPosDes=#{current_user_cp}&cTipoEnvio=#{@product.tipo_envio}&cIdioma=Esp&nPeso=#{@product.peso}&nLargo=#{@product.largo}&nAncho=#{@product.ancho}&nAlto=#{@product.alto}"
+            end
+            logger.debug "#{url}\n\n\n\n\n\n"
+            require 'net/http'
+            require 'rubygems'
+            require 'nokogiri'
+            require 'open-uri'
+
+             doc = Nokogiri::HTML(open(url))
+             @dias = []
+             doc.css(':nth-child(6) .style5 strong , :nth-child(7) strong, :nth-child(8) strong').each do |node|
+                  @dias.push(node.text)
+             end 
+             logger.debug "#{@dias}\n\n\n\n\n\n"
+
+             @tarifas = []
+                doc.css(':nth-child(6) td:nth-child(8) , :nth-child(7) :nth-child(8), :nth-child(8) td:nth-child(8)').each do |node|
+                @tarifas.push(node.text)
+             end
+             logger.debug "#{@tarifas}\n\n\n\n\n\n"
          end
-         logger.debug "#{url}\n\n\n\n\n\n"
-         require 'net/http'
-         require 'rubygems'
-         require 'nokogiri'
-         require 'open-uri'
-
-          doc = Nokogiri::HTML(open(url))
-          @dias = []
-          doc.css(':nth-child(6) .style5 strong , :nth-child(7) strong, :nth-child(8) strong').each do |node|
-               @dias.push(node.text)
-          end 
-          logger.debug "#{@dias}\n\n\n\n\n\n"
-
-          @tarifas = []
-             doc.css(':nth-child(6) td:nth-child(8) , :nth-child(7) :nth-child(8), :nth-child(8) td:nth-child(8)').each do |node|
-             @tarifas.push(node.text)
-          end
-          logger.debug "#{@tarifas}\n\n\n\n\n\n"
       end
-      
       respond_to do |format|
         format.html # show.html.erb
         format.json { render json: @product }
