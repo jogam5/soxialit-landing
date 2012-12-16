7518cc33a7e430b72e8b772710f24d301a5dc597
julio <julio336@hotmail.com>
2012-12-16 17:26:01 -0600

    validacion del tamano de las imagenes

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -3,6 +3,11 @@ class Painting < ActiveRecord::Base
   mount_uploader :image, ProductPictureUploader
   
   belongs_to :product
-  validates_presence_of :image
+  
+  validate :picture_size_validation, :if => "image?"  
+  
+  def picture_size_validation
+      errors[:image] << "la imagen debe ser menor a 1MB, intenta con otra imagen" if image.size > 1.megabytes
+  end
   
 end
