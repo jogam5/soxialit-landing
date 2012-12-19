80a08e92aa02c53d0d0afd529e99abd402581597
jogam5 <jogam5@yahoo.com.mx>
2012-12-18 21:09:09 -0600

    ability microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
+  #load_and_authorize_resource
 
   def new
     @micropost = Micropost.new
