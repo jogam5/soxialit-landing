5207e2ecb3e2d305c7528694c6f503efa051e1eb
jogam5 <jogam5@yahoo.com.mx>
2012-12-31 12:37:19 -0600

    after_save in model fixed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,4 @@
 class ActivitiesController < ApplicationController
-  #after_save :expire_get_feed_cache
-
   def create
-  end
-
-  def expire_get_feed_cache
-  	Rails.cache.delete('get_feed')
-  	return true
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,10 +4,17 @@ class Activity < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :as => :commentable, :dependent => :destroy
 
+  after_save :expire_get_feed_cache
+
+
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
           user_id: user.id)
+  end
+
+  def expire_get_feed_cache
+    Rails.cache.delete('get_feed')
   end
 end
