4f37efde6ff53e9626974d178486360c245c8e3b
jogam5 <jogam5@yahoo.com.mx>
2012-12-31 12:05:25 -0600

    update cache in activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,10 @@
 class ActivitiesController < ApplicationController
+  after_save :expire_get_feed_cache
+
   def create
+  end
+
+  def expire_get_feed_cache
+  	Rails.cache.delete('get_feed')
   end
 end
