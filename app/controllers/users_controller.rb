31b5ab9976289cf0ab028a40236a873e6c93e3ef
jogam5 <jogam5@yahoo.com.mx>
2013-01-04 23:48:59 -0600

    filters for followers, etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, except: [:show, :list_items, :favorites, :followers]
+  before_filter :authenticate_user!, :except => [:show, :list_items, :favorites, :followers, :following]
   load_and_authorize_resource
 
   def index
