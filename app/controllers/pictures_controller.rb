02d9eac81cbe206695960bd893aaa62328109248
julio <julio336@hotmail.com>
2013-01-08 12:22:08 -0600

    permisos projects

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,7 @@
 class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
+=begin
   def index
     @pictures = Picture.all
 
@@ -78,3 +79,49 @@ class PicturesController < ApplicationController
     end
   end
 end
+=end
+
+def new
+   @project = Project.find(params[:project_id]) 
+   @picture = @project.pictures.build
+
+   respond_to do |format|
+     format.html # new.html.erb
+     format.json { render json: @picture }
+   end
+ end
+ 
+ def destroy
+     @picture = Picture.find(params[:id])
+     @picture.destroy
+
+     respond_to do |format|
+       format.html { redirect_to pictures_url }
+       format.json { head :no_content }
+       format.js
+     end
+   end
+ end
+ 
+ def update
+     @picture = Picture.find(params[:id])
+
+     respond_to do |format|
+       if @picture.update_attributes(params[:picture])
+         format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+         format.json { head :no_content }
+       else
+         format.html { render action: "edit" }
+         format.json { render json: @picture.errors, status: :unprocessable_entity }
+       end
+     end
+   end
+   
+   def create
+       @picture = Picture.create(params[:picture])
+       @project = Project.find(@picture.project_id)
+       picture_first = @project.pictures.first.image_url(:timeline).to_s
+       logger.debug "picture: #{picture_first}\n\n\n\n\n\n"
+       @project.update_attribute(:picture, picture_first)
+     end
+ 
