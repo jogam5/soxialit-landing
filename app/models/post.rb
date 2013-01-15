fcfe647db8f31cd3cb3826eaa48320d2ea79ac2b
jogam5 <jogam5@yahoo.com.mx>
2013-01-15 00:47:07 -0600

    msg

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,9 +28,9 @@ class Post < ActiveRecord::Base
         rescue Koala::Facebook::APIError => e
             #puts ex.message
           if e.message.include?("OAuthException: Error validating access token: Session does not match current stored session.")
-            Rails.logger.error "Facebook access token not valid"
-            @api = Koala::Facebook::OAuth.new("235628993153454", "6dc90b8b268f2643ebd5b074a88db7c8")
-            @api.exchange_access_token_info(@user.token)
+            Rails.logger.error "Facebook access token not valid damn"
+            @rapi = Koala::Facebook::OAuth.new("235628993153454", "6dc90b8b268f2643ebd5b074a88db7c8")
+            @rapi.exchange_access_token_info(@user.token)
             options = {
               :message => "Acabo de publicar un nuevo Micropost en Soxialit.",
               :picture => @post.slides.first.picture.to_s,
@@ -38,7 +38,7 @@ class Post < ActiveRecord::Base
               :name => "#{@post.title} by #{@post.user.nickname}",
               :description => @post.quote
             }
-            @api.put_connections("me", "feed", options)
+            @rapi.put_connections("me", "feed", options)
           else
             Rails.logger.error "FacebookApi#perform Koala Error with #{e}, model_id:#{model.id}"
           end
