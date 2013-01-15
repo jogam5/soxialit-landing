1b0e34b6ba3d4939147aba7c06a4ce7ed0dbf1a1
julio <julio336@hotmail.com>
2013-01-14 18:36:59 -0600

    project lect list

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,10 @@ class Picture < ActiveRecord::Base
   
   mount_uploader :image, ProjectPictureUploader
   
+  validate :picture_size_validation, :if => "image?", :on => :create
+   
+  def picture_size_validation
+     errors[:image] << "la imagen debe ser menor a 2MB, intenta con otra imagen" if image.size > 2.megabytes
+  end
+  
 end
