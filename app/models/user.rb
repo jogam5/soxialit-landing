6ba5f25f01d4de23087f0246bac94c89c7d07e63
jogam5 <jogam5@yahoo.com.mx>
2013-01-16 19:23:46 -0600

    more

--- app/models/user.rb
+++ app/models/user.rb
@@ -149,7 +149,7 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    @feed = Activity.from_users_followed_by(self).order("created_at DESC").limit(35)
+    @feed = Activity.from_users_followed_by(self).order("created_at DESC").limit(45)
   end
 
   def feed_cached
