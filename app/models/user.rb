1ed4cd42429ef589c1011c29e3966fb46799db0f
jogam5 <jogam5@yahoo.com.mx>
2013-01-27 23:19:10 -0600

    silencer for cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -151,7 +151,7 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    @feed = Activity.from_users_followed_by(self).order("created_at DESC").limit(95)
+    @feed = Activity.from_users_followed_by(self).order("created_at DESC").limit(100)
   end
 
   def feed_cached
