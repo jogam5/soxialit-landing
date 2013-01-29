d82a0195adbb15b00ea62662fb3177354cb3d2ff
jogam5 <jogam5@yahoo.com.mx>
2013-01-29 13:33:29 -0600

    cache delete in controllers

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,12 @@
 class ActivitiesController < ApplicationController
+  after_save :expire_feed_cache, :only => :create
+  after_destroy :expire_feed_cache
+
   def create
+  end
+
+  def expire_feed_cache
+  	user = current_user
+    Rails.cache.delete('feed_user_#{user.id}')
   end
 end
