6929834c53a0a872cd6930cb8e93c9edae8a9720
jogam5 <jogam5@yahoo.com.mx>
2013-02-01 17:04:55 -0600

    newrel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,12 +4,10 @@ class StaticPagesController < ApplicationController
   	@comment = Comment.new
     @micropost = Micropost.new
   	if user_signed_in?
-      logger.debug "here the cache goes"
   		#@feed_items = User.feed_cached(current_user)  if Rails.env.production?
       @feed_items = current_user.feed 
       
       #@feed_items = User.feed_cached(current_user)
-      logger.debug "el current_user id: #{current_user.id}"
   	else
   	  #@feed_items = User.feed_cached(User.find(1)) if Rails.env.production?
       #@feed_items = User.feed_cached(User.find(47))
