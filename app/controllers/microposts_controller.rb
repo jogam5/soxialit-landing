0ee74ef314d895223cdb2204d812c657c916125c
julio <julio336@hotmail.com>
2013-02-11 20:08:30 -0600

    index

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -11,7 +11,7 @@ class MicropostsController < ApplicationController
   end
   
   def index  
-     @microposts = Micropost.page(params[:page]).per_page(10)
+     @microposts = Micropost.page(params[:page]).per_page(20)
   end
 
   def show
