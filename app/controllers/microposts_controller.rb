5b8c3fadcdbffe0dc572d07d334d1c07749607d7
julio <julio336@hotmail.com>
2013-02-21 17:27:08 -0600

    index

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -11,7 +11,7 @@ class MicropostsController < ApplicationController
   end
   
   def index  
-    @microposts = Micropost.page(params[:page]).per_page(50).find_with_reputation(:lovs, :all, order: 'lovs desc')
+     @microposts = Micropost.page(params[:page]).per_page(50).find(:all, :order => "created_at DESC")
   end
   
   def microposts_lov
