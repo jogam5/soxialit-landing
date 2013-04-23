ac4ac72e4eb692017707cebe11352aac3410f8e3
julio <julio336@hotmail.com>
2013-04-23 01:08:49 -0500

    db

--- db/schema.rb
+++ db/schema.rb
@@ -365,13 +365,6 @@ ActiveRecord::Schema.define(:version => 20130418042457) do
     t.integer  "micropost_id"
   end
 
-  create_table "sourceships", :force => true do |t|
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.integer  "micropost_id"
-    t.integer  "source_id"
-  end
-
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
