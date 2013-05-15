6df5035a625b81981f54a62c95ed0b24028012d2
jogam5 <jogam5@yahoo.com.mx>
2013-05-15 01:26:44 -0500

    last nw

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -14,8 +14,4 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   version :thumb_profile do
     process :resize_and_pad => [50,50,"#240902"]
   end
-
-  version :recreo do
-    process :resize_and_pad => [200,200,"#240902"]
-  end
 end
