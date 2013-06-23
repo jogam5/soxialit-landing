7732565f26ae0ced348fdf350047d52f6c17950b
jogam5 <jogam5@yahoo.com.mx>
2013-06-22 20:29:16 -0500

    ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :redirect_ssl
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to user_path(current_user), :alert => exception.message
 	exception.default_message = "blage"
   end
+
+  private 
+
+  #only need this until Google doesn't link to HTTPS anymore
+  def redirect_ssl
+    if request.ssl?
+      redirect_to "http://#{request.host_with_port}#{request.fullpath}", 
+                  :status => :moved_permanently
+    end
+  end
+
 end
