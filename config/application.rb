db79e12f7c06c1e35428d28ed1ff48b800dd30c3
jogam5 <jogam5@yahoo.com.mx>
2013-06-22 19:55:20 -0500

    no crt2

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module DeviseFacebook
     #      rack_env['SERVER_NAME'] == 'www.soxialit.com'
     #    }
     #end
-
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
